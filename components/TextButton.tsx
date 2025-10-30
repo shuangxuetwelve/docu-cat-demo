39099c9618ee7c2bd0d128cdf3f04a5819c13d1e
DocuCat <docu-cat@users.noreply.github.com>
2025-10-30 08:41:15 +0000

    docs: Update documentation based on code changes
    Updated by DocuCat AI assistant based on recent code changes.
    
    Documents updated:
      - components/ButtonSmall.tsx
      - components/TextButton.tsx
      - README.md
      - docs/COMPONENTS.md

--- components/TextButton.tsx
+++ components/TextButton.tsx
@@ -16,7 +16,7 @@ interface TextButtonProps {
  * A text-styled button component that looks like a clickable text link.
  * Perfect for subtle actions, inline links, or when you need a button that doesn't stand out.
  *
- * TODO: A better implementation is to use the DocuCatButton component as a base and set the variant to "text". But here we leave it as a future improvement.
+ * TODO: A better implementation is to use the ButtonBase component as a base and set the variant to "text". But here we leave it as a future improvement.
  *
  * @param props - TextButton component props
  * @returns A button component styled as text
